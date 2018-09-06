d4d3fbb5aebf38cf44ca423dfc15276908db2cdf
ren <1101553279@qq.com>
2018-09-06 14:24:57 +0000

    update 32_to_16.c

--- 32_to_16.c
+++ 32_to_16.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
-
+typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 
+u8 con_16_to_8( u16 va_16 )
+{
+    u8 va_8;
+    u8 i = 0;
+
+    for( i = 0; i < 8; i++ )
+    {
+        if( 1 == (0x1 & ( va_16 >> (2*i+1) ) ) )
+            va_8 |= 1<<i;
+        else
+            va_8 &= ~(1<<i);
+    }
+
+    return va_8;
+}
+
 u16 con_32_to_16( u32 va_32 )
 {
-    u16 va_16;
+    u16 va_16 = 0;
     u16 i = 0;
 
     for( i = 0; i < 16; i++ )
@@ -23,12 +39,16 @@ u16 con_32_to_16( u32 va_32 )
 
 int main( void )
 { 
+    u8 va_8;
     u16 va_16;
-    u32 va_32 = 0b11111111111101010000000000000000;
+    u32 va_32 = 0x33553355;
 
     va_16 = con_32_to_16( va_32 );
 
     printf( " va_16 = %x\n", va_16 );
+
+    va_8 = con_16_to_8( va_16 );
+    printf( " va_8 = %x\n", va_8 );
 
     return 0;
 }
